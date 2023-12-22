abbba5fa5cbc00c3243654ca2b31a39224f2b98c
Yodya Mahesa <yodyamahesa123@gmail.com>
2023-12-22 13:20:11 +0800

    Machine Learning Commit

--- predik.py
+++ predik.py
@@ -2,9 +2,9 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from tensorflow.keras.models import load_model
 
-kelas_label=['celana_panjang', 'celana_pendek', 'kemeja', 'sepatu', 'skirt', 'sweater_and_jacket', 't-shirt']
+kelas_label=['celana_panjang', 'celana_pendek', 'kemeja', 'skirt', 'sweater_and_jacket', 't-shirt']
 
-img = image.load_img('rok3.jpeg', target_size=(150,150))
+img = image.load_img('contoh.jpg', target_size=(224,224))
 
 # Ubah gambar ke dalam bentuk array numerik
 img_array = image.img_to_array(img)
@@ -12,7 +12,7 @@ img_array = image.img_to_array(img)
 # Sesuaikan dimensi gambar untuk input model
 img_array = np.expand_dims(img_array, axis=0)
 
-model=load_model('model_coba.h5')
+model=load_model('model_coba4.h5')
 
 prediksi=model.predict(img_array)
 
